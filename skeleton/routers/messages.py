c25572ef699662be33e664b6423224b9fc6edab9
Ina Ivanova <iivanova308@outlook.com>
2024-05-03 01:08:52 +0300

    Adding message routers and services

--- skeleton/routers/messages.py
+++ skeleton/routers/messages.py
@@ -1,20 +1,27 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-
+from data.models import Message
+from services.messages_service import create_message, get_conversation, get_all_conversations
 
 messages_router = APIRouter(prefix="/messages/users")
 
 
-@messages_router.post("/{sender_id}/{receiver_id}")
-def create_new_message():
-    pass
+@messages_router.post("/{sender_id}/{receiver_id}", response_model=Message)
+def create_new_message(sender_id: int, receiver_id: int, message: Message):
+
+    new_message = create_message(message.text, sender_id, receiver_id)
+    return new_message
 
 
-@messages_router.get("/{sender_id}/{receiver_id}")
-def get_conversation():
-    pass
+@messages_router.get("/{sender_id}/{receiver_id}", response_model=list[Message])
+def get_conversation(sender_id: int, receiver_id: int):
+
+    conversation = get_conversation(sender_id, receiver_id)
+    return conversation
 
 
-@messages_router.get("/{sender_id}/receivers")
-def get_all_conversations():
-    pass
+@messages_router.get("/{sender_id}/receivers", response_model=list[Message])
+def get_all_conversations(sender_id: int):
+
+    conversations = get_all_conversations(sender_id)
+    return conversations
