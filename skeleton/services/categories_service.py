1ae922bcf764849efb25b609b4fef143d69a1b87
Kaloyan Nikolov <kknikolov8@gmail.com>
2024-05-02 17:12:26 +0300

    Added search and sort query params to the get_category_by_id service method.

--- skeleton/services/categories_service.py
+++ skeleton/services/categories_service.py
@@ -14,11 +14,21 @@ def get_all_categories() -> list[Category] or None:
     return categories
 
 
-def get_category_by_id(category_id: int) -> Category or None:  # todo
+def get_category_by_id(category_id: int, search: str = None, sort: str = None) -> Category or None:  # todo
     category_query = """select category_id, name, is_locked, is_private from categories where category_id = ?"""
     topic_query = """select topic_id, title, category_id, user_id, creation_date, best_reply, is_locked from topics 
     where category_id = ?"""
     params = (category_id,)
+
+    if search:
+        category_query += " and name like ?"
+        params += (f'%{search}%',)
+
+    if sort:
+        if sort.lower() in ["asc", "desc"]:
+            category_query += f" order by category_id {sort.lower()}"
+        else:
+            raise ValueError("Sorting categories only using 'asc' or 'desc'!")
 
     category_data = read_query(category_query, params)
     topics_data = read_query(topic_query, params)
