824c1e07f32345cec4e555f69398d0d76a340591
Kaloyan Nikolov <kknikolov8@gmail.com>
2024-05-09 01:57:12 +0300

    Added some additional logic to the get_conversation router method.

--- skeleton/routers/messages.py
+++ skeleton/routers/messages.py
@@ -3,7 +3,7 @@ from common.responses import InternalServerError
 from services import messages_service
 from data.models import CreateMessage
 from common.authentication import get_user_or_raise_401
-from common.responses import Conflict
+from common.responses import Conflict, NotFound
 
 messages_router = APIRouter(prefix="/messages/users")
 
@@ -27,17 +27,24 @@ def create_new_message(receiver_id: int, creating_message: CreateMessage, token:
 @messages_router.get("/{receiver_id}")
 def get_conversation(receiver_id: int, token: str = Header()):
 
-    sender_id = get_user_or_raise_401(token).id
+    sender = get_user_or_raise_401(token)
 
-    conversation = messages_service.get_conversation(sender_id, receiver_id)
+    if sender.id == receiver_id:
+        return Conflict(content="Sender and receiver cannot be the same user")
+
+    conversation = messages_service.get_conversation(sender.id, receiver_id)
+
+    if not conversation:
+        return NotFound(content=f"There is no conversation going on with user with id:{receiver_id}!")
+
     return conversation
 
 
 @messages_router.get("/receivers")
 def get_all_conversations(token: str = Header()):
 
-    sender_id = get_user_or_raise_401(token).id
+    sender = get_user_or_raise_401(token)
 
-    conversations = messages_service.get_all_conversations(sender_id)
+    conversations = messages_service.get_all_conversations(sender.id)
 
     return conversations
