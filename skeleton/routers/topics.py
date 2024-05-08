73b01223dc4df50cc3f5847a2fa90de13d448264
Kaloyan Nikolov <kknikolov8@gmail.com>
2024-05-09 01:18:05 +0300

    More refactoring for better coherence.

--- skeleton/routers/topics.py
+++ skeleton/routers/topics.py
@@ -17,11 +17,11 @@ def get_all_topics(search: Optional[str] = None, sort_by: Optional[str] = None,
     return topics
 
 
-@topics_router.get("/{id}")
-def get_topic_by_id(id: int):
-    topic = topics_services.get_topic_with_replies(id)
+@topics_router.get("/{topic_id}")
+def get_topic_by_id(topic_id: int):
+    topic = topics_services.get_topic_with_replies(topic_id)
     if not topic:
-        return NotFound(content=f"Topic with id:{id} does not exist!")
+        return NotFound(content=f"Topic with id:{topic_id} does not exist!")
     return topic
 
 
@@ -36,14 +36,14 @@ def create_new_topic(creating_topic: CreateTopic, token: str = Header()):
     return new_topic
 
 
-@topics_router.put("/{id}/lock")
-def lock_topic(id: int, token: str = Header()):
+@topics_router.put("/{topic_id}/lock")
+def lock_topic(topic_id: int, token: str = Header()):
     user = get_user_or_raise_401(token)
     if not user.is_admin:
         return Forbidden(content="Only admins can lock topics")
-    topic = topics_services.get_topic(id)
+    topic = topics_services.get_topic(topic_id)
     if not topic:
         return NotFound(content="Topic not found")
 
-    topics_services.lock_topic(id)
-    return {"message": f"Topic with id:{id} locked successfully"}
+    topics_services.lock_topic(topic_id)
+    return {"message": f"Topic with id:{topic_id} locked successfully"}
